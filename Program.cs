a8cd012754575d645dcfe901e138d02bb8ad5ce2
Giovani da Silva Ramos <gi_1997@hotmail.com>
2022-05-23 01:02:37 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using jogo.src.Entities;
+
+namespace jogo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight arus = new Knight("Arus", 23, "Knight", 70, 20);
+            Wizard ally = new Wizard("Jenica", 23, "White Mage", 40, 60);
+            Ninja ally2 = new Ninja("Naruto", 23, "Ninja", 50, 30);
+            Wizard ally3 = new Wizard("Jerico", 23, "Black Mage", 40, 60);
+
+            Console.WriteLine(arus);
+            Console.WriteLine(ally);
+            Console.WriteLine(ally2);
+            Console.WriteLine(ally3);
+            Console.WriteLine(arus.Attack());
+            Console.WriteLine(ally.Attack(3));
+            Console.WriteLine(ally2.Attack());
+            Console.WriteLine(ally3.Attack(7));
+
+
+        }
+    }
+}
---
+++ src/Entities/Hero.cs
@@ -0,0 +1,38 @@
+namespace jogo.src.Entities
+{
+    public abstract class Hero
+    {
+
+        public Hero(string Name, int Level, string HeroType, int HP, int MP){
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+            this.HP = HP;
+            this.MP = MP;
+
+        }
+
+        public Hero(){
+            
+        }
+
+        public string Name;
+        public int Level;
+        public string HeroType;
+        public int HP;
+        public int MP;
+
+
+
+        public override string ToString(){
+            return "Nome: "+ this.Name + " Level: " + this.Level + " Classe: " + this.HeroType  + " HP: " + this.HP  + " MP: " + this.MP;
+        }
+
+        public virtual string Attack(){
+            return this.Name + " Atacou com a sua espada!";
+        }
+
+
+
+    }
+}
